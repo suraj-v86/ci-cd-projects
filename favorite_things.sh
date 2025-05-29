9ba3fa258b955d56853bd784c1ddccfce4610746
suraj <suraj@example.com>
2025-05-29 13:27:27 +0200

    seventh commit

---
+++ favorite_things.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# collect the input from the user
+
+colour=${COLOUR}
+
+food=${FOOD}
+
+movie=${MOVIE}
+
+echo ""
+
+echo "You love $colour, enjoy eating $food, and like watching $movie."
